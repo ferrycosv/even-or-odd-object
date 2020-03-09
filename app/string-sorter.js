b5e9852ed1a07a727bc202076a7b3a4abe9c041e
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2020-03-09 20:42:59 +0100

    initial

---
+++ app/string-sorter.js
@@ -0,0 +1,14 @@
+const stringSorter = {
+  evens: [],
+  odds: [],
+  NaNy: [],
+  addString: function (newString) {
+    // add the new string to the correct array
+  },
+  getByType: function (typeOfString) {
+    // return a copy of the correct array
+  },
+  getAllString: function () {
+    // return a new array with all saved strings
+  }
+}
