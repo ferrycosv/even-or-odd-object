f6bdb54f1efc81d51d61850b573781b6d589ecd1
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2020-03-10 19:11:21 +0100

    refactor-to-render-methods

--- app/string-sorter.js
+++ app/string-sorter.js
@@ -1,14 +1,12 @@
 const stringSorter = {
+  active: 'evens',
   evens: [],
   odds: [],
   NaNy: [],
   addString: function (newString) {
     // add the new string to the correct array
   },
-  getByType: function (typeOfString) {
-    // return a copy of the correct array
-  },
-  getAllString: function () {
-    // return a new array with all saved strings
+  renderList: function () {
+    // return the active array rendered to a list
   }
 }
